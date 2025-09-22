75948b07f5c1a06ee5a41c74f124323bde74eaf5
Franciele-Santos-Silva <franciellysantos876@gmail.com>
2025-09-22 12:36:13 -0300

    Erro no os.path

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 from PIL import Image
+import os
 
+pasta = 'images'
+
+lista_arquivos = os.listdir(pasta)
+
+for nome_arquivo in lista_arquivos:
+    imagem = Image.open(os.path.join(pasta, nome_arquivo))
+
+    novo_nome = os.path.join(pasta, f'reduzido_ {nome_arquivo}')
+    imagem.save(novo_nome, optimize= True)
